d92356df54134c9d1a0ae15b8cf437c6f0c4f847
fernando.mendoza <fernando.mendoza@salesforce.com>
2022-08-16 12:01:22 -0700

    add an example json response

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 6066
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+    .get('/api/auth', (req, res) => {
+        console.log(res.console);
+        console.log(req.console);
+
+        let testJson = {
+          userToken: "asdasd",
+          passToke: "secret"
+        };
+
+        // res.send('Hello World!');
+        res.send(testJson);
+    })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
