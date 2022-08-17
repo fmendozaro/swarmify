0ca217a0229542535befd5ac43c2e9702e4ad964
fernando.mendoza <fernando.mendoza@salesforce.com>
2022-08-17 15:34:23 -0700

    update: better format for link and info

--- index.js
+++ index.js
@@ -26,9 +26,7 @@ express()
 
         res.send(testJson);
     })
-    .post('/api/get-pending', (request, response) => {
-
-        // response.set('Content-Type', 'application/x-www-form-urlencode')
+    .get('/api/get-pending', (request, response) => {
 
         getCollaborators();
 
@@ -47,36 +45,13 @@ express()
                         "response_type": "in_channel",
                         "text": responseText
                     }
+
                     response.send(finalResponse);
 
-                    // data.forEach(members => {
-                    //     getReviews(members.login);
-                    // })
                 })
                 .catch(error => {
                     response.send(error);
                 });
         }
-
-        function getPullRequests(){
-            axios.get(`https://api.github.com/repos/${username}/${repo}/pulls`, commonHeader)
-                .then(results => {
-                    response.json(results.data);
-                })
-                .catch(error => {
-                    response.send(error);
-                });
-        }
-
-        function getReviews(currentUser){
-            axios.get(`https://api.github.com/repos/${currentUser}/${repo}/pulls?state=all`, commonHeader)
-                .then(results => {
-                    response.json(results.data);
-                })
-                .catch(error => {
-                    response.send(error);
-                });
-        }
-
     })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
